ec89a15c52f1e293560226c29743e778f8258587
krishnendu basak <krisauz17@yahoo.com>
2022-03-09 01:01:46 +1100

    What are Tests is done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,21 +4,21 @@ const { name, height, message } = require("../index.js");
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
+      expect(index.name).toEqual('Susan')
     })
   })
 
 
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
+      expect(index.height).toBeLessThan(40)
     })
   })
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(message).toInclude(name)
-      expect(message).toInclude(height)
+      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
     })
   })
 })
